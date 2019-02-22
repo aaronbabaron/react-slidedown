5e0be48ddb3054502394b65081b18fc4e4e2c1ac
Aaron Min <amin@netflix.com>
2019-02-22 11:07:20 -0800

    Change to react memo

--- lib/slidedown.d.ts
+++ lib/slidedown.d.ts
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from "react";
 interface SlideDownProps {
+    children?: React.ReactNode;
     className?: string;
     transitionOnAppear?: boolean;
     closed?: boolean;
 }
-export declare const SlideDown: React.ComponentType<SlideDownProps & React.ClassAttributes<HTMLDivElement>>;
+export declare const SlideDown: React.MemoExoticComponent<React.ForwardRefExoticComponent<SlideDownProps & React.RefAttributes<HTMLDivElement>>>;
 export default SlideDown;
